43665fe7e52b15c899f4ac1579f02a979ea93d8c
Hypnotic_Melody <gdscottcarino@gmail.com>
2025-11-05 15:22:03 -0800

    ezPrint function made!!!! woo!!!!!! oh and some playing around with the stacks

--- pushingP/pushDown.cpp
+++ pushingP/pushDown.cpp
@@ -1,15 +1,34 @@
 #include <iostream>
 #include <vector>
 
+// IT'S EZPRINT TIME!!! WOOO!!! (it can handle any dimension vector, along with whatever type that can be normally printed by cout!)
+template <typename T>
+void ezPrint(T data) {
+    std::cout << data;
+}
 
+template <typename T>
+void ezPrint(std::vector<T> data, bool spacing = false) {
+    std::cout << "{";
+    if (spacing) { std::cout << "\n";}
+    for (size_t i = 0; i < data.size(); i++) {
+        ezPrint(data[i]);
+        if (spacing) { std::cout << "\n"; }
+        else if (i < data.size() - 1) { std::cout << ","; }
+    }
+    std::cout << "}";
+}
+// END OF EZPRINT TIME!!!
 
+template <typename T>
 struct Stack {
     size_t size = 0;
-    std::vector<int> data = {};
+    std::vector<T> data = {};
 
-    void push(int e) {
+    void push(T e) {
         data.push_back(e);
         size = size + 1;
+        std::cout << "Pushed on: " << e << "\n";
         return;
     }
 
@@ -17,28 +36,42 @@ struct Stack {
         size = size - 1;
         int value = data[size];
         data.pop_back();
+        std::cout << "Popped off: " << value << "\n";
         return value;
     }
 
     void print() {
-        std::cout << "{";
-        for (size_t i = 0; i < size; i++) {
-            std::cout << data[i] << ", ";
-        }
-        std::cout << "}\n";
+        ezPrint(data, false);
+        std::cout << "\n";
         return;
     }
 };
 
 int main() {
-    Stack s; // stuck at array size 5 :(
-    s.push(5);
-    s.push(6);
+    Stack<int> intStack;
+    intStack.push(5);
+    intStack.push(6);
 
-    s.print();
-    std::cout << s.pop() << "\n";
-    s.print();
-    std::cout << s.pop() << "\n";
-    s.print();
+    intStack.print();
+    intStack.pop();
+    intStack.print();
+    intStack.pop();
+    intStack.print();
+
+    Stack<const char*> charStack;
+    charStack.push("H");
+    charStack.push("e");
+    charStack.push("l");
+    charStack.push("l");
+    charStack.push("o");
+    charStack.push(" ");
+    charStack.push("W");
+    charStack.push("o");
+    charStack.push("r");
+    charStack.push("l");
+    charStack.push("d");
+    charStack.push("!");
+
+    charStack.print();
     return 0;
 }
